5161addbeb52da33faa219075a23a8100f89121e
Julien Andrew <julienandrew1998@gmail.com>
2020-09-21 22:30:45 -0400

    removed rating field from review model

--- eBIRProject/src/app/models/review.ts
+++ eBIRProject/src/app/models/review.ts
@@ -4,6 +4,5 @@ import { Brewery } from './brewery';
 export class Review {
     public submitter: User;
     public brewery: Brewery;
-    public rating: number;
     public reviewText: string;
 }
--- eBIRProject/src/app/pages/brewerypage/brewerypage.component.ts
+++ eBIRProject/src/app/pages/brewerypage/brewerypage.component.ts
@@ -138,7 +138,6 @@ export class BrewerypageComponent implements OnInit {
     const review: Review = {
         submitter : user,
         brewery : this.brewery,
-        rating: this.rating,
         reviewText : this.reviewText
       };
 
